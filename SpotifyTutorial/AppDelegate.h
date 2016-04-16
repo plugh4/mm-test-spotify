b2c1106f7b32cbd97b5d754f03a1a1a2d76f1274
michaelmerrill <michael@michaelmerrill.io>
2016-04-16 15:07:46 -0700

    Initial Commit

---
+++ SpotifyTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotifyTutorial
+//
+//  Created by Michael Merrill on 4/16/16.
+//  Copyright © 2016 Michael Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpotifyTutorial/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SpotifyTutorial
+//
+//  Created by Michael Merrill on 4/16/16.
+//  Copyright © 2016 Michael Merrill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
